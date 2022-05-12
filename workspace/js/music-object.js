39210340d7d8b8216d4930519771a8177e2119b6
= <=>
2022-05-12 20:26:55 +0900

    곡 오브젝트 수정

--- workspace/js/music-object.js
+++ workspace/js/music-object.js
@@ -1,295 +1,295 @@
-class MusicItem extends HTMLDivElement{
+class MusicItem extends HTMLDivElement {
   constructor() {
-    super()
-    this.context = '';
+    super();
+    this.context = "";
+    this._builder = this._builder.bind(this);
+    this._setInnerText = this._setInnerText.bind(this);
+    this._updateLikeStatus = this._updateLikeStatus.bind(this);
   }
-  
+
   setup(obj) {
-    this.setAttribute('music-id', obj.id);
-    this.setAttribute('song-title', obj.title);
-    this.setAttribute('song-artist', obj.artist);
-    this.setAttribute('duration', obj.duration);
-    this.setAttribute('is-liked', obj.isLiked);
-    this.setAttribute('is-new', obj.isNew);
+    this.classList.add("music-item");
+    this.setAttribute("music-id", obj.id);
+    this.setAttribute("song-title", obj.title);
+    this.setAttribute("song-artist", obj.artist);
+    this.setAttribute("duration", obj.duration);
+    this.setAttribute("is-liked", obj.isLiked);
+    this.setAttribute("is-new", obj.isNew);
     this.referencedObj = obj;
-    
-    this.setInnerText(obj.title, obj.artist, obj.duration);
+
+    this._setInnerText(obj.title, obj.artist, obj.duration);
   }
-  
-  setInnerText(title, artist, duration) {
-    const shadow = this.shadowRoot;
-    let songTitle = shadow.querySelector('.song-title');
-    let songArtist = shadow.querySelector('.song-artist');
-    let songDuration = timeFormatter(duration, 'duration');
-    
-    songTitle.setAttribute('title', title);
+
+  _setInnerText(title, artist, duration) {
+    let songTitle = this.querySelector(".song-title");
+    let songArtist = this.querySelector(".song-artist");
+    let songDuration = this._timeFormatter(duration, "duration");
+
+    songTitle.setAttribute("title", title);
     songTitle.innerHTML = title;
-    songArtist.innerHTML = artist + ' · ' + songDuration;
+    songArtist.innerHTML = artist + " · " + songDuration;
   }
-  
-  updateLikeStatus() {
-    let id = this.getAttribute('music-id');
+
+  _updateLikeStatus() {
+    let id = this.getAttribute("music-id");
     let musics = document.querySelectorAll(`[music-id=${id}]`);
-    let currentLike = this.getAttribute('is-liked');
-    
-    musics.forEach(item => {
-      if (item.getAttribute('music-id') == currentLike) return;
-      item.setAttribute('music-id', currentLike);
-    })
+    let currentLike = this.getAttribute("is-liked");
+
+    musics.forEach((item) => {
+      if (item.getAttribute("music-id") == currentLike) return;
+      item.setAttribute("music-id", currentLike);
+    });
   }
-  
+
   static get musicItem() {
     return this.referencedObj;
+  }
+
+  _builder(type) {
+    //기본 구조 생성
+    const frontIndicator = document.createElement("div");
+    const songInfo = document.createElement("div");
+    const buttonField = document.createElement("div");
+    frontIndicator.classList.add("front-indicator");
+    frontIndicator.setAttribute("data-index", "");
+    songInfo.classList.add("song-info");
+    buttonField.classList.add("button-field");
+
+    //곡 정보 영역 생성
+    const songTitle = document.createElement("strong");
+    const songArtist = document.createElement("span");
+    songTitle.classList.add("song-title");
+    songArtist.classList.add("song-artist");
+    songInfo.append(songTitle, songArtist);
+
+    //버튼 생성
+    switch (type) {
+      case "playable":
+        const meatballs = document.createElement("button");
+        const addButton = document.createElement("button");
+        meatballs.classList.add("music-meatballs");
+        addButton.classList.add("music-add");
+        buttonField.append(meatballs, addButton);
+        break;
+
+      case "queue":
+      case "edit":
+        const dragButton = document.createElement("div");
+        const deleteButton = document.createElement("button");
+        dragButton.classList.add("music-drag");
+        deleteButton.classList.add("music-delete");
+        buttonField.append(dragButton, deleteButton);
+        break;
+
+      case "selectable":
+        const selectButton = document.createElement("button");
+        selectButton.classList.add("music-select");
+        buttonField.append(selectButton);
+        break;
+    }
+
+    this.append(frontIndicator, songInfo, buttonField);
+  }
+
+  _timeFormatter(time, type) {
+    if (type == "duration") {
+      let min = parseInt(time / 60);
+      let sec = time % 60;
+
+      if (min < 10) min = "0" + min;
+      if (sec < 10) sec = "0" + sec;
+
+      return min + ":" + sec;
+    }
   }
 }
 
 class PlayableItem extends MusicItem {
   constructor() {
     super();
-    
-    shadowMaker.call(this, 'playable');
-    const addButton = this.shadowRoot.querySelector('.music-add');
-    
-    addButton.onclick = this.addToPlaylistForButton.bind(this);
-    this.onclick = this.clickToPlay.bind(this);
+
+    this._builder("playable");
+    const addButton = this.querySelector(".music-add");
+
+    addButton.onclick = this._addToPlaylistForButton.bind(this);
+    this.onclick = this.__clickToPlay.bind(this);
   }
-  
+
   setup(obj) {
     super.setup(obj);
-    if (obj.isPlaying) this.classList.add('playing');
+    if (obj.isPlaying) this.classList.add("playing");
   }
-  
-  clickToPlay(e) {
+
+  __clickToPlay(e) {
     if (e.defaultPrevented) return;
-    
-    this.sendToPlay(this.referencedObj, this.context);
+
+    this._sendToPlay(this.referencedObj, this.context);
   }
-  
-  addToPlaylistForButton(e) {
+
+  _addToPlaylistForButton(e) {
     e.preventDefault();
-    
-    playlistManager.addToPlaylist([this.referencedObj]); //플레이리스트 매니저의 플레이리스트 추가 함수
+
+    // playlistManager.addToPlaylist([this.referencedObj]); // 플레이리스트 매니저의 플레이리스트 추가 함수
   }
-  
-  sendToPlay(music, context) {
-    //재생 대기열에 정보를 보내는 메소드.
-    console.log('sended');
+
+  _sendToPlay(obj, context) {
+    // 재생 대기열에 정보를 보내는 메소드.
+    // queueManager.playThis(obj, context);
+    console.log("sended");
   }
 }
 
 class LibraryItem extends PlayableItem {
   constructor() {
-    super()
-    
-    const meatballs = this.shadowRoot.querySelector('.music-meatballs');
-    meatballs.onclick = this.openContextMenu.bind(this);
-    
-    this.context = 'Library';
-    this.oncontextmenu = this.openContextMenu.bind(this);
+    super();
   }
-  
-  setup(obj) {
+
+  setup(obj, isRecord = false) {
     super.setup(obj);
-    this.classList.add('library')
+    this.classList.add("library");
+
+    if (!isRecord) {
+      this.context = "Library";
+    } else {
+      this.context = "Record";
+    }
+
+    const meatballs = this.querySelector(".music-meatballs");
+    let contextMenu = this._openContextMenu.bind(this);
+    meatballs.onclick = contextMenu;
+    this.oncontextmenu = contextMenu;
   }
-  
-  openContextMenu(e) {
+
+  _openContextMenu(e) {
     e.preventDefault();
-    
-    let contextMenu = document.createElement('library-context');
+
+    /* let contextMenu = document.createElement("library-context");
     contextMenu.setup(this);
-    this.append(contextMenu);
+    this.append(contextMenu); */ // 컨텍스트 메뉴 만들어 추가.
   }
 }
 
 class PlaylistItem extends PlayableItem {
   constructor() {
-    super()
-    
-    let meatballs = this.shadowRoot.querySelector('.music-meatballs');
-    
-    meatballs.onclick = this.openContextMenu.bind(this);
-    this.context = 'Playlist:';
-    this.oncontextmenu = this.openContextMenu.bind(this);
+    super();
+
+    this.context = "Playlist:";
   }
-  
-  setup(obj, context, index='') {
+
+  setup(obj, context, index = "") {
     super.setup(obj);
+
     this.context += context;
     this.index = index;
-    this.shadowRoot.querySelector('.front-indicator').innerHTML = index;
-    this.classList.add('playlist')
+    this.querySelector(".front-indicator").innerHTML = index;
+    this.classList.add("playlist");
+
+    let meatballs = this.querySelector(".music-meatballs");
+    let contextMenu = this._openContextMenu.bind(this);
+    meatballs.onclick = contextMenu;
+    this.oncontextmenu = contextMenu;
   }
-  
+
   updateIndex(index) {
     if (this.index == index) return;
     this.index = index;
-    this.shadowRoot.querySelector('.front-indicator').innerHTML = index;
+    this.querySelector(".front-indicator").innerHTML = index;
   }
-  
-  openContextMenu(e) {
+
+  _openContextMenu(e) {
     e.preventDefault();
-    
-    let contextMenu = document.createElement('playlist-context');
+
+    /* let contextMenu = document.createElement("playlist-context");
     contextMenu.setup(this);
-    this.append(contextMenu);
+    this.append(contextMenu); */ // 컨텍스트 메뉴 생성
   }
 }
 
 class QueueItem extends MusicItem {
   constructor() {
-    super()
-    
-    shadowMaker.call(this, 'queue');
-    let deleteButton = this.shadowRoot.querySelector('.music-delete');
-    
-    deleteButton.onclick = this.deleteMusic.bind(this);
-    this.onclick = this.clickToPlay.bind(this);
-    this.oncontextmenu = this.openContextMenu.bind(this);
+    super();
+
+    this._builder("queue");
+    let deleteButton = this.querySelector(".music-delete");
+
+    deleteButton.onclick = this._deleteMusic.bind(this);
+    this.onclick = this._clickToPlay.bind(this);
+    this.oncontextmenu = this._openContextMenu.bind(this);
   }
-  
+
   setup(obj, context) {
     super.setup(obj);
-    
-    if (obj.isPlaying) this.classList.add('playing');
+
+    if (obj.isPlaying) this.classList.add("playing");
     this.context = context;
-    this.classList.add('queue')
+    this.classList.add("queue");
   }
-  
-  clickToPlay(e) {
+
+  _clickToPlay(e) {
     if (e.defaultPrevented) return;
-    
-    queueManager.playQueue(this.referencedObj, this.context); //큐매니저에서 재생을 맡김.
+
+    // queueManager.playQueue(this.referencedObj, this.context); // 큐매니저에서 재생을 맡김.
   }
-  
-  deleteMusic(e) {
+
+  _deleteMusic(e) {
     e.preventDefault();
-    
+
     this.remove();
   }
-  
-  openContextMenu(e) {
+
+  _openContextMenu(e) {
     e.preventDefault();
-    
-    let contextMenu = document.createElement('queue-context');
+
+    /* let contextMenu = document.createElement("queue-context");
     contextMenu.setup(this);
-    this.append(contextMenu);
+    this.append(contextMenu); */
   }
 }
 
 class EditingItem extends MusicItem {
   constructor() {
-    super()
-    
-    shadowMaker.call(this, 'edit');
-    let deleteButton = this.shadowRoot.querySelector('.music-delete');
-    
-    deleteButton.onclick = this.deleteMusic.bind(this);
+    super();
+
+    this._builder("edit");
+    let deleteButton = this.querySelector(".music-delete");
+
+    deleteButton.onclick = this._deleteMusic.bind(this);
   }
-  
-  deleteMusic(e) {
-    e.preventDefault();
-    
-    this.remove();
-  }
-  
+
   setup(obj) {
     super.setup(obj);
-    this.classList.add('editing')
+    this.classList.add("editing");
+  }
+
+  _deleteMusic(e) {
+    e.preventDefault();
+
+    this.remove();
   }
 }
 
 class SelectableItem extends MusicItem {
   constructor() {
-    super()
-    shadowMaker.call(this, 'selectable');
+    super();
+    this._builder("selectable");
     this.isSelected = false;
-    this.onclick = this.toggleSelectedStatus.bind(this);
+    this.onclick = this._toggleSelectedStatus.bind(this);
   }
-  
+
   setup(obj) {
     super.setup(obj);
-    this.classList.add('selectable')
+    this.classList.add("selectable");
   }
-  
-  toggleSelectedStatus(e) {
+
+  _toggleSelectedStatus(e) {
     this.isSelected = !this.isSelected;
-    this.classList.toggle('selected');
+    this.classList.toggle("selected");
     // this.index = selectableManager.addOrClear(this.referencedObj, this.index) => 인덱스가 undefined이면 스택에 추가, 새로운 인덱스를 반환, 인덱스가 있으면 해당 인덱스의 아이템 삭제 후 undefined 반환.
   }
 }
 
-customElements.define('library-item', LibraryItem, {extends: 'div'});
-customElements.define('playlist-item', PlaylistItem, {extends: 'div'});
-customElements.define('queue-item', QueueItem, {extends: 'div'});
-customElements.define('editing-item', EditingItem, {extends: 'div'});
-customElements.define('selectable-item', SelectableItem, {extends: 'div'});
-
-function timeFormatter(time, type) {
-  if (type == 'duration') {
-    let min = parseInt(time/60);
-    let sec = time%60;
-    
-    if (min < 10) min = '0' + min;
-    if (sec < 10) sec = '0' + sec;
-    
-    return min + ':' + sec;
-  }
-}
-
-function shadowMaker(type) {
-  let shadow;
-  
-  if (!this.shadowRoot) {
-    shadow = this.attachShadow({mode: 'open'});
-  } else {
-    shadow = this.shadowRoot;
-  }
-  
-  //기본 구조 생성
-  const frontIndicator = document.createElement('div');
-  const songInfo = document.createElement('div');
-  const buttonField = document.createElement('div');
-  const styleObj = shadowStyle.content.cloneNode(true);
-  frontIndicator.classList.add('front-indicator');
-  frontIndicator.setAttribute('data-index', '');
-  songInfo.classList.add('song-info');
-  buttonField.classList.add('button-field');
-  
-  //곡 정보 영역 생성
-  const songTitle = document.createElement('strong');
-  const songArtist = document.createElement('span');
-  songTitle.classList.add('song-title');
-  songArtist.classList.add('song-artist');
-  songInfo.append(songTitle, songArtist);
-  
-  //버튼 생성
-  switch(type) {
-    case 'playable' :
-      const meatballs = document.createElement('button');
-      const addButton = document.createElement('button');
-      meatballs.classList.add('music-meatballs');
-      addButton.classList.add('music-add');
-      buttonField.append(meatballs, addButton);
-      break;
-
-    case 'queue' :
-    case 'edit' : 
-      const dragButton = document.createElement('div');
-      const deleteButton = document.createElement('button');
-      dragButton.classList.add('music-drag');
-      deleteButton.classList.add('music-delete');
-      buttonField.append(dragButton, deleteButton);
-      break;
-      
-    case 'selectable' : 
-      const selectButton = document.createElement('button');
-      selectButton.classList.add('music-select');
-      buttonField.append(selectButton);
-      break;
-  };
-  
-  shadow.append(frontIndicator, songInfo, buttonField, styleObj);
-  
-  return;
-}
+customElements.define("library-item", LibraryItem, { extends: "div" });
+customElements.define("playlist-item", PlaylistItem, { extends: "div" });
+customElements.define("queue-item", QueueItem, { extends: "div" });
+customElements.define("editing-item", EditingItem, { extends: "div" });
+customElements.define("selectable-item", SelectableItem, { extends: "div" });
